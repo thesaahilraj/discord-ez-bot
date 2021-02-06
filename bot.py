0e309b75ff39a9eeb52f74b595c9a906db83bb95
Saahil Raj <54501769+thesaahilraj@users.noreply.github.com>
2021-02-06 20:37:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,16 +6,6 @@ from discord.abc import Messageable
 
 client = Bot(command_prefix=">")
 
-
-def read_token():
-    with open("token.txt", "r") as f:
-        lines = f.readlines()
-        return lines[0].strip()
-
-
-token = read_token()
-
-
 @client.event
 async def on_ready():
     print("Bot is Ready")
