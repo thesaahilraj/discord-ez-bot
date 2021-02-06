8df60bbe952def0cc41b6200b063c862007e6540
thesaahilraj <saahilraj@hotmail.com>
2021-02-06 20:06:19 +0530

    Added token

--- bot.py
+++ bot.py
@@ -6,7 +6,14 @@ from discord.abc import Messageable
 
 client = Bot(command_prefix=">")
 
-token = "ODA3Mjg4MDYwOTcwMDA4NjA3.YB1zmA.PewKX6-SXPQaZ404vswKxGBcmwo"
+
+def read_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+
+token = read_token()
 
 
 @client.event
